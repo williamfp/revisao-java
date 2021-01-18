0715180dc5e0aecb244e8c76adc31db5613c836f
williamfp <william.passini@gmail.com>
2021-01-18 14:26:24 -0300

    Main view design

--- javafx-jdbc-project/src/application/Main.java
+++ javafx-jdbc-project/src/application/Main.java
@@ -2,8 +2,8 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,9 +14,13 @@ public class Main extends Application {
     public void start(Stage primaryStage){
         try{
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-            Parent parent = loader.load();
+            ScrollPane scrollPane = loader.load();
 
-            Scene mainScene = new Scene(parent);
+            //Auto adjust height and width of scrollpane
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+
+            Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
