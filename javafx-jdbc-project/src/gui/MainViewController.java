5899afa3794ab04a3bc6d552254dfbdda7ee1f9f
williamfp <william.passini@gmail.com>
2021-01-18 16:57:00 -0300

    Initializing action as parameter
    Note: the original version (i.e., proposed by the instructor) does not
    use method overloading. Instead, whenever a controller is not needed, an
    empty function (i.e., x -> {}) is provided as a parameter. The version
    in this commit encapsulates this by overloading the loadView method

--- javafx-jdbc-project/src/gui/MainViewController.java
+++ javafx-jdbc-project/src/gui/MainViewController.java
@@ -16,6 +16,7 @@ import model.services.DepartmentService;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 public class MainViewController implements Initializable {
 
@@ -34,7 +35,10 @@ public class MainViewController implements Initializable {
     }
     @FXML
     public void onMenuItemDeparmentAction(){
-        loadView2("/gui/DepartmentList.fxml");
+        loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        });
     }
     @FXML
     public void onMenuItemAboutAction(){
@@ -46,25 +50,13 @@ public class MainViewController implements Initializable {
 
     }
 
-    private synchronized void loadView(String absoluteName) {
-        try{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-            VBox newVbox = loader.load();
-
-            Scene mainScene = Main.getMainScene();
-            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-            Node mainMenu = mainVbox.getChildren().get(0);
-            mainVbox.getChildren().clear();
-
-            mainVbox.getChildren().add(mainMenu);
-            mainVbox.getChildren().addAll(newVbox.getChildren());
-        } catch (IOException e){
-            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
-        }
+    //Method used whenever loading a view that does not require an action to be executed
+    private synchronized void loadView(String absoluteName){
+        this.loadView(absoluteName, x -> {});
     }
 
-    private synchronized void loadView2(String absoluteName) {
+    //Method used whenever loading a view that does require an action to be executed
+    private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
         try{
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             VBox newVbox = loader.load();
@@ -78,9 +70,12 @@ public class MainViewController implements Initializable {
             mainVbox.getChildren().add(mainMenu);
             mainVbox.getChildren().addAll(newVbox.getChildren());
 
-            DepartmentListController controller = loader.getController();
-            controller.setDepartmentService(new DepartmentService());
-            controller.updateTableView();
+            //Returns the controller that corresponds to the type passed from the Consumer function
+            T controller = loader.getController();
+
+            // Executes the function passed via lambda function
+            initializingAction.accept(controller);
+
         } catch (IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
