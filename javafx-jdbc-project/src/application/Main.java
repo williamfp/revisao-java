7f327b35696b239685400d29b1c2817fc9904d50
williamfp <william.passini@gmail.com>
2021-01-18 14:21:11 -0300

    Main view

--- javafx-jdbc-project/src/application/Main.java
+++ javafx-jdbc-project/src/application/Main.java
@@ -1,12 +1,28 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            Parent parent = loader.load();
+
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 
