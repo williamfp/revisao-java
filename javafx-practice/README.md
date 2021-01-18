03bf58822ec18bd5d5074382917f3a83302205af
williamfp <william.passini@gmail.com>
2021-01-18 13:40:42 -0300

    Creating and using a ComboBox. Add README

---
+++ javafx-practice/README.md
@@ -0,0 +1,13 @@
+# Interface gráfica com JavaFX
+
+## Objetivos
++ Configurar um ambiente de trabalho que suporte a utilização de JavaFX
++ Aprender alguns dos componentes básicos de maneira prática, através da criação de um projeto exemplo. Este repositório contempla exemplos simples de:
+	+ Criação de uma tela em branco
+	+ Implementação de uma classe controladora
+	+ Tratamento de eventos
+	+ Criação e exibição de janelas de alerta customizadas
+	+ Criação de um TextField e aplicação em um exemplo de calculadora de soma de dois números ponto-flutuante de precisão dupla (*double*)
+	+ Criação de limitações (*constraints*) para permitir inclusão apenas de números na aplicação de cálculo de soma
+	+ Implementação da interface `Initializable` para aplicar as limitações na interface
+	+ Utilização de ComboBox
--- javafx-practice/src/gui/ViewController.java
+++ javafx-practice/src/gui/ViewController.java
@@ -1,55 +1,63 @@
 package gui;
 
-import gui.util.Alerts;
-import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
 
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ViewController implements Initializable {
-    @FXML
-    private TextField txtNumber1;
 
     @FXML
-    private TextField txtNumber2;
+    private ComboBox<Person> comboBoxPerson;
 
     @FXML
-    private Label labelResult;
+    private Button btAll;
+
+    private ObservableList<Person> obsList;
 
     @FXML
-    private Button btSum;
+    public void onComboBoxPersonAction(){
+        Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
+        System.out.println(person);
+    }
 
     @FXML
-    public void onBtSumAction(){
-        Locale.setDefault(Locale.US);
-
-        try{
-            double number1 = Double.parseDouble(txtNumber1.getText());
-            double number2 = Double.parseDouble(txtNumber2.getText());
-
-            double sum = number1 + number2;
-
-            labelResult.setText(String.format("%.2f", sum));
-        } catch (NumberFormatException e){
-            Alerts.showAlert("Error", "ParseError", e.getMessage(), Alert.AlertType.ERROR);
+    public void onBtAllAction(){
+        for(Person p : comboBoxPerson.getItems()){
+            System.out.println(p);
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //Only double digits
-        Constraints.setTextFieldDouble(txtNumber1);
-        Constraints.setTextFieldDouble(txtNumber2);
+        List<Person> list = new ArrayList<>();
+        list.add(new Person(1, "Maria", "maria@gmail.com"));
+        list.add(new Person(2, "Alex", "alex@gmail.com"));
+        list.add(new Person(3, "Bob", "bob@gmail.com"));
 
-        //Max length = 12
-        Constraints.setTextFieldMaxLength(txtNumber1, 12);
-        Constraints.setTextFieldMaxLength(txtNumber2, 12);
+        obsList = FXCollections.observableArrayList(list);
+        comboBoxPerson.setItems(obsList);
+
+        Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+            @Override
+            protected void updateItem(Person item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty ? "" : item.getName());
+            }
+        };
+
+        comboBoxPerson.setCellFactory(factory);
+        comboBoxPerson.setButtonCell(factory.call(null));
     }
 }
