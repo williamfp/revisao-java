d085677dfb9e36a7b9208956b5c6b692bfdb99b9
williamfp <william.passini@gmail.com>
2021-01-18 13:14:51 -0300

    TextField and calculator example

--- javafx-practice/src/gui/ViewController.java
+++ javafx-practice/src/gui/ViewController.java
@@ -4,17 +4,37 @@ import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class ViewController {
     @FXML
-    private Button btTest;
+    private TextField txtNumber1;
 
     @FXML
-    public void onBtTestAction(){
-        //More complete version (with header)
-        //Alerts.showAlert("Alert title", "Alert header", "Hello", Alert.AlertType.INFORMATION);
+    private TextField txtNumber2;
 
-        //Simplified version (without header)
-        Alerts.showAlert("Alert title", null, "Hello", Alert.AlertType.INFORMATION);
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+    public void onBtSumAction(){
+        Locale.setDefault(Locale.US);
+
+        try{
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+
+            double sum = number1 + number2;
+
+            labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e){
+            Alerts.showAlert("Error", "ParseError", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
