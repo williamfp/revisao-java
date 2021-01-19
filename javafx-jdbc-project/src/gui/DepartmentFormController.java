e0c3e3e26c55fa1232d0261b865bc5169dfe2c8b
williamfp <william.passini@gmail.com>
2021-01-19 11:21:21 -0300

    Saving a new Department
    NOTE: there is a possible optimization during the data retrieval from
    the form fields. More details regarding this are expressed as a comment
    on gui.DepartmentFormController class

--- javafx-jdbc-project/src/gui/DepartmentFormController.java
+++ javafx-jdbc-project/src/gui/DepartmentFormController.java
@@ -1,12 +1,18 @@
 package gui;
 
+import db.exceptions.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +20,8 @@ import java.util.ResourceBundle;
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -34,14 +42,53 @@ public class DepartmentFormController implements Initializable {
         this.entity = entity;
     }
 
-    @FXML
-    public void onBtSaveAction(){
-        System.out.println("onBtSaveAction");
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
     }
 
     @FXML
-    public void onBtCancelAction(){
-        System.out.println("onBtCancelAction");
+    public void onBtSaveAction(ActionEvent event){
+        if(entity == null){
+            throw new IllegalStateException("Entity was null");
+        }
+
+        if(service == null){
+            throw new IllegalStateException("Service was null");
+        }
+
+        try{
+            /*
+            * The following parameter and the getFormData() method can be optimized
+            * A new instance of DepartmentFormController already gives an instance
+            * of Department via the variable entity, hence, there is no need to create
+            * a new Department on the method getFormData, only to set the values to the
+            * already existing entity object
+            *
+            * I've decided to preserve the original code to reflect what's on the course,
+            * source files, but to insert this note to remember this possible optimization.
+            */
+            entity = getFormData();
+
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        } catch (DbException e){
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+
+        return obj;
+    }
+
+    @FXML
+    public void onBtCancelAction(ActionEvent event){
+        Utils.currentStage(event).close();
     }
 
     @Override
