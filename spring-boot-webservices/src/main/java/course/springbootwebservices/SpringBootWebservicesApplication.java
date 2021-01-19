5403de438808c2d885323b51e4acbb3e625b5e0b
williamfp <william.passini@gmail.com>
2021-01-19 16:46:52 -0300

    Project created

---
+++ spring-boot-webservices/src/main/java/course/springbootwebservices/SpringBootWebservicesApplication.java
@@ -0,0 +1,13 @@
+package course.springbootwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebservicesApplication.class, args);
+    }
+
+}
---
+++ spring-boot-webservices/src/test/java/course/springbootwebservices/SpringBootWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package course.springbootwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
