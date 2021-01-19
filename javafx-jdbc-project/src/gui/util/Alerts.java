e8d14c202611eb3c3038912f89ebf6049e8d78b2
williamfp <william.passini@gmail.com>
2021-01-19 12:22:17 -0300

    Remove department

--- javafx-jdbc-project/src/gui/util/Alerts.java
+++ javafx-jdbc-project/src/gui/util/Alerts.java
@@ -1,6 +1,9 @@
 package gui.util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class Alerts {
     public static void showAlert(String title, String header, String content, Alert.AlertType type){
@@ -9,5 +12,13 @@ public class Alerts {
         alert.setHeaderText(header);
         alert.setContentText(content);
         alert.show();
+    }
+
+    public static Optional<ButtonType> showConfirmation(String title, String content){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        return alert.showAndWait();
     }
 }
