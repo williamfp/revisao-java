e6496c10aa66daa150fb123fb6f14b1ea30fdbb6
williamfp <william.passini@gmail.com>
2021-01-19 18:34:56 -0300

    Service layer, component registration

--- spring-boot-webservices/src/main/java/course/springbootwebservices/repositories/UserRepository.java
+++ spring-boot-webservices/src/main/java/course/springbootwebservices/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package course.springbootwebservices.repositories;
 
 import course.springbootwebservices.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
