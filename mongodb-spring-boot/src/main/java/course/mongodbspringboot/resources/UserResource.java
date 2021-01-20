9b65d563e1407279c039df8cc45789efa4a73ade
William <william.passini@gmail.com>
2021-01-20 17:36:31 -0300

    Updating user by id

--- mongodb-spring-boot/src/main/java/course/mongodbspringboot/resources/UserResource.java
+++ mongodb-spring-boot/src/main/java/course/mongodbspringboot/resources/UserResource.java
@@ -50,4 +50,13 @@ public class UserResource {
 
         return ResponseEntity.noContent().build();
     }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        service.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
 }
