582f013c96dd097d205009898d662b53bdbd2eeb
William <william.passini@gmail.com>
2021-01-20 18:56:27 -0300

    Adding comments to posts

--- mongodb-spring-boot/src/main/java/course/mongodbspringboot/domain/Post.java
+++ mongodb-spring-boot/src/main/java/course/mongodbspringboot/domain/Post.java
@@ -1,11 +1,14 @@
 package course.mongodbspringboot.domain;
 
 import course.mongodbspringboot.dto.AuthorDTO;
+import course.mongodbspringboot.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -19,6 +22,8 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
     }
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
