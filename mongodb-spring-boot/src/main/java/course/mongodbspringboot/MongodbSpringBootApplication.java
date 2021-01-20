352d870d1b4d5b3f4fc5585a8824e173ae2b6b92
William <william.passini@gmail.com>
2021-01-20 16:21:22 -0300

    Project created

---
+++ mongodb-spring-boot/src/main/java/course/mongodbspringboot/MongodbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package course.mongodbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbSpringBootApplication.class, args);
+    }
+
+}
---
+++ mongodb-spring-boot/src/test/java/course/mongodbspringboot/MongodbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package course.mongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
