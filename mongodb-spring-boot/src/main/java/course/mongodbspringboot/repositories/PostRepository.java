917997976b462488cfe65c2d662afbf9adbf138c
William <william.passini@gmail.com>
2021-01-20 19:13:02 -0300

    Custom search using Query

--- mongodb-spring-boot/src/main/java/course/mongodbspringboot/repositories/PostRepository.java
+++ mongodb-spring-boot/src/main/java/course/mongodbspringboot/repositories/PostRepository.java
@@ -2,9 +2,13 @@ package course.mongodbspringboot.repositories;
 
 import course.mongodbspringboot.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 }
