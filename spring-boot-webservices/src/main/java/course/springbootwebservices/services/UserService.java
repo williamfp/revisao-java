90bfceef86793152d35e6dbe98878a390aba7aed
williamfp <william.passini@gmail.com>
2021-01-20 10:45:30 -0300

    Exception handling - delete

--- spring-boot-webservices/src/main/java/course/springbootwebservices/services/UserService.java
+++ spring-boot-webservices/src/main/java/course/springbootwebservices/services/UserService.java
@@ -2,8 +2,11 @@ package course.springbootwebservices.services;
 
 import course.springbootwebservices.entities.User;
 import course.springbootwebservices.repositories.UserRepository;
+import course.springbootwebservices.services.exceptions.DatabaseException;
 import course.springbootwebservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
